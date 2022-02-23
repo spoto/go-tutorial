bc62baec930be54f12d9444c9a973acb798bcd3f
Fausto Spoto <fausto.spoto@gmail.it>
2022-02-23 11:00:30 +0100

    Added other examples from Chapter 4

--- ch4/issues/main.go
+++ ch4/issues/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/spoto/go-tutorial/ch4/github"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,7 +14,29 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
+	fmt.Printf("\nLess than a month old:\n")
+	now := time.Now()
+	oneMonthAgo := now.AddDate(0, -1, 0)
+	oneYearAgo := now.AddDate(-1, 0, 0)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		if item.CreatedAt.After(oneMonthAgo) {
+			print(item)
+		}
 	}
+	fmt.Printf("\nLess than a year old:\n")
+	for _, item := range result.Items {
+		if !item.CreatedAt.After(oneMonthAgo) && item.CreatedAt.After(oneYearAgo) {
+			print(item)
+		}
+	}
+	fmt.Printf("\nMore than a year old:\n")
+	for _, item := range result.Items {
+		if !item.CreatedAt.After(oneYearAgo) {
+			print(item)
+		}
+	}
+}
+
+func print(item *github.Issue) (int, error) {
+	return fmt.Printf("#%-5d %s %9.9s %.55s\n", item.Number, item.CreatedAt.Format("[02/01/2006]"), item.User.Login, item.Title)
 }
