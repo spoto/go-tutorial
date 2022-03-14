49038d8d3a417df8c1bfb3db2d8cebcdb606ab6d
Fausto Spoto <fausto.spoto@gmail.it>
2022-03-14 11:30:31 +0100

    Started examples from Chapter 8

---
+++ ch8/clock3/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+var port = flag.Int("port", 8000, "port number")
+
+func main() {
+	flag.Parse()
+	address := fmt.Sprintf("localhost:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
