577dd754cd2cd27745210de5c8732327c7189a91
Fausto Spoto <fausto.spoto@gmail.it>
2022-03-22 18:22:39 +0100

    Added examples from Chapter 11

---
+++ ch11/storage1/storage_test.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCheckQuotaNotifiesUser(t *testing.T) {
+	var notifiedUser, notifiedMsg string
+	saved := notifyUser
+	defer func() { notifyUser = saved }()
+	notifyUser = func(user, msg string) {
+		notifiedUser, notifiedMsg = user, msg
+	}
+
+	const user = "joe@example.org"
+	usage[user] = 980000000
+
+	CheckQuota(user)
+	if notifiedUser != user {
+		t.Errorf("wrong user (%s) notified, want %s", notifiedUser, user)
+	}
+	const wantSubstring = "98% of your quota"
+	if !strings.Contains(notifiedMsg, wantSubstring) {
+		t.Errorf("unexpected notification message <<%s>>, "+
+			"want substring %q", notifiedMsg, wantSubstring)
+	}
+}
